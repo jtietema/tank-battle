fd195167e24cdb99794e526b9aaf0b26fbe6d19c
Maik Gosenshuis <maik@gosenshuis.nl>
2009-03-24 16:14:55 +0100

    Server now saves state of the tanks.

--- tank_battle/server/server.py
+++ tank_battle/server/server.py
@@ -2,21 +2,29 @@ from hulknet.server.gameserver import GameServerApp
 from tank_battle.server.protocol import TankBattleServerProtocol
 from tank_battle.server.game import TankBattleGame
 
-class TankBattleServer(GameServerApp):
+class TankBattleServer(GameServerApp):    
     def __init__(self, port):
         GameServerApp.__init__(self, port, TankBattleServerProtocol, TankBattleGame(self))
         self.currentId = 0
+        self.last_known_states = {}
     
     def tankState(self, id, rot, (x,y), source_player):
         '''Send the message back to all clients'''
+        self.last_known_states[id] = (rot, x, y)
+        
         for player in self.players:
             if player is not source_player:
                 player.protocol.sendTankState(id, rot, (x, y))
     
     def tankId(self, player):
+        for id, (rot, x, y) in enumerate(self.last_known_states):
+            player.protocol.sendTankState(id, rot, (x, y))
+        
         self.currentId += 1
         player.protocol.sendTankId(self.currentId)
     
     def tankRemove(self, id):
+        del self.last_known_states[id]
+        
         for player in self.players:
             player.protocol.sendTankRemove(id)
