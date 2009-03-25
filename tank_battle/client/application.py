6381117bfd5d7bd82a558d74fe093e4b974415f2
Jeroen Tietema <jeroen@tietema.net>
2009-03-25 16:19:09 +0100

    - AI tank players are now updated

--- tank_battle/client/application.py
+++ tank_battle/client/application.py
@@ -15,7 +15,7 @@ class TankBattleClient(GenericClientApp):
         GenericClientApp.__init__(self,clientFactoryClass)
         self.players = {}
         self.tank_layer = None
-        self.computer_players = {}
+        self.computer_players = []
     
     def run(self):
         director.init(caption='Tank Battle', width=800, height=600)
@@ -63,6 +63,7 @@ class TankBattleClient(GenericClientApp):
             self.scroller.add(self.tank_layer)
         else:
             ai_tank = ComputerTank(id, ((self.current_map.px_width // 2), (self.current_map.px_height // 2)), self)
+            self.computer_players.append(ai_tank)
             self.tank_layer.add(ai_tank)
     
     def serverTankRemove(self, id):
