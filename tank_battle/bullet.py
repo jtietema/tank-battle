780b0d4d4d85d7d817d1d4c6e05ab0fecb85ca08
Maik Gosenshuis <maik@gosenshuis.nl>
2009-03-26 15:22:48 +0100

    Bullets are now removed when they are outside the map

--- tank_battle/bullet.py
+++ tank_battle/bullet.py
@@ -8,7 +8,8 @@ SPEED = 400
 
 
 class Bullet(Sprite):
-    def __init__(self, id, rotation, pos):
+    def __init__(self, id, rotation, pos, app):
+        self.app = app
         self.id = id
         
         self.pos = pos
@@ -24,4 +25,13 @@ class Bullet(Sprite):
         
         self.x += math.sin(r) * s
         self.y += math.cos(r) * s
+        
+        left = self.x - self.image_anchor_x
+        right = self.x + self.image_anchor_x
+        
+        top = self.y + self.image_anchor_y
+        bottom = self.y - self.image_anchor_y
+        
+        if top < 0 or right < 0 or left > self.app.current_map.px_width or bottom > self.app.current_map.px_height:
+            self.app.remove_bullet(self.id)
         
